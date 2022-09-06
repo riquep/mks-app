b9bdda4ae93ac8a870ef3c71108ed9e0f3f852c6
Henrique Lima <henrique.lima@tst.jus.br>
2022-09-06 16:46:12 -0300

    Ajuste Env Var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://rebrtsisodcrodspzlnw.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlYnJ0c2lzb2Rjcm9kc3B6bG53Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MTI4MDAzMCwiZXhwIjoxOTc2ODU2MDMwfQ.HetyWbiFSw47NLZSgzyeXV-xoqQNrXhpycU8-s2jH4Q',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlYnJ0c2lzb2Rjcm9kc3B6bG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEyODAwMzAsImV4cCI6MTk3Njg1NjAzMH0.5PBnnWh3kjE3m-9Mg_bm32DALqtlufVrPIUb_ED7T4w',
 };
